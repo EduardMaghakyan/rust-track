bb7e9cd424f37a8a7ebdf40b0d916a302db67e09
Eduard Maghakyan <edi.maghakyan@gmail.com>
2020-04-01 21:59:32 +0200

    Improve performance and memory usage

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,25 +2,32 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut word_letters = HashMap::new();
-    let word = word.to_lowercase();
-    for c in word.chars() {
-        *word_letters
-            .entry(c.escape_unicode().to_string())
-            .or_insert(1) += 1;
+    let word_lower = word.to_lowercase();
+    let mut word_freq = HashMap::new();
+    for c in word_lower.chars() {
+        *word_freq.entry(c.escape_unicode().to_string()).or_insert(0) += 1;
     }
 
     let mut anagrams: HashSet<&'a str> = HashSet::new();
     // let mut anagrams = HashSet::new();
     for possible in possible_anagrams {
-        let mut possible_letters = HashMap::new();
-        for c in possible.to_lowercase().chars() {
-            *possible_letters
+        // if words have different length they can't be anagrams
+        if possible.chars().count() != word.chars().count() {
+            continue;
+        }
+        let mut possible_freq = word_freq.clone();
+        let possible_lower = possible.to_lowercase();
+        for c in possible_lower.chars() {
+            if !possible_freq.contains_key(&c.escape_unicode().to_string()) {
+                break;
+            }
+            *possible_freq
                 .entry(c.escape_unicode().to_string())
-                .or_insert(1) += 1;
+                .or_insert(1) -= 1;
         }
 
-        if word_letters == possible_letters && word != possible.to_lowercase() {
+        possible_freq.retain(|_, v| *v != 0);
+        if possible_freq.is_empty() && word_lower != possible_lower {
             anagrams.insert(possible);
         }
     }
